a83fbb815acc9214d706f7bfaa360a3326d2dda6
ALonso2001 <alonso05rr01@gmail.com>
2024-08-28 22:07:33 -0600

    Cambios en el modulo principal: module_gtb

--- design/module_gtb.sv
+++ design/module_gtb.sv
@@ -1,25 +1,32 @@
 module gray_to_binary(
-    //entrada gray y salida binario
-    input logic [3:0] gray_in
+    // Entrada gray y salida binario
+    input logic [3:0] gray_in,
     output logic [3:0] binario_out
 );
 
-function logic [3:0] gray_to_binary(input logic [3:0] gray);
-    logic [3:0] binario;
-    
-    //MSB del código gray es igual el del código binario
-    binario[3] = gray[3]
+    // Función para convertir código Gray a binario
+    function logic [3:0] gray_to_binary(input logic [3:0] gray);
+        logic [3:0] binario;
+        
+        // MSB del código Gray es igual al del código binario
+        binario[3] = gray[3];
 
-    //binario[2] 
-    binario[2] = (gray[3] & ~(gray[2])) | (~(gray[3]) & gray[2]);
+        // binario[2] 
+        binario[2] = (gray[3] & ~(gray[2])) | (~(gray[3]) & gray[2]);
 
-    //binario[1]
-    binario[1] = (~(binario[2]) & gray[1]) | ((binario[2]) & ~(gray[1]));
+        // binario[1]
+        binario[1] = (~(binario[2]) & gray[1]) | ((binario[2]) & ~(gray[1]));
 
-    //binario[0]
-    binario[0] = (binario[1] & ~(gray[0])) | (~(binario[1]) & gray[0]);
+        // binario[0]
+        binario[0] = (binario[1] & ~(gray[0])) | (~(binario[1]) & gray[0]);
 
-    return binario;
-end function
+        return binario;
+    endfunction
+
+    // Bloque combinacional para llamar a la función y asignar el resultado
+    always_comb begin
+        binario_out = gray_to_binary(gray_in);
+    end
+endmodule
 
 
