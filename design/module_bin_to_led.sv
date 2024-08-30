691f1b858caa605f5b63f7520c1e9bcecd8b9fe3
ALonso2001 <alonso05rr01@gmail.com>
2024-08-29 21:30:50 -0600

    Arreglos constraints

--- design/module_bin_to_led.sv
+++ design/module_bin_to_led.sv
@@ -1,8 +1,8 @@
 module bin_to_led(
     input logic [3:0] bin_in, // Entrada binaria 
-    output logic [3:0] leds   //Salida a los LEDs
+    output logic [3:0] led   //Salida a los LEDs
 );
 
 // Asignacion directa de binario a los leds
-    assign leds = bin_in;
+    assign led = bin_in;
 endmodule 
