9acd700b44b93f3728490d6cd9335fba4c206f64
ALonso2001 <alonso05rr01@gmail.com>
2024-08-30 21:52:43 -0600

    Ajuste al top module

--- design/module_gray_decoder_system.sv
+++ design/module_gray_decoder_system.sv
@@ -1,4 +1,4 @@
-module gray_decoder_system(
+/*module gray_decoder_system(
     input logic [3:0] gray_in, // Entrada codigo gray
     output logic [3:0] led, // Salida a los leds de la FPGA
     output logic [6:0] seg // SAlida al 7 segmentos 
@@ -21,4 +21,35 @@ module gray_decoder_system(
         .bin_in(bin_out),
         .seg(seg)
     );
+endmodule*/
+
+module gray_decoder_system(
+    input logic [3:0] gray_in,  // Entrada codigo gray
+    output logic [3:0] led,     // Salida a los LEDs de la FPGA
+    output logic [6:0] seg,     // Salida común a ambos displays
+    output logic [1:0] an       // Anodos para seleccionar el display
+);
+
+    // Señal interna para la salida binaria del subsistema 
+    logic [3:0] bin_out;
+
+    // Instanciar el módulo que convierte Gray a binario
+    gray_to_binary gray_to_binary_inst (
+        .gray_in(gray_in),
+        .binario_out(bin_out)
+    );
+
+    // Instanciar el módulo que controla los LEDs
+    bin_to_led bin_to_led_inst (
+        .bin_in(bin_out),
+        .led(led)
+    );
+
+    // Instanciar el módulo que controla los displays de 7 segmentos y los anodos
+    bin_to_7seg bin_to_7seg_inst (
+        .bin_in(bin_out),
+        .seg(seg),
+        .an(an)
+    );
+
 endmodule
